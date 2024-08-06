122b0e777743875d73d9d17446aca4d63e5e0231
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-08-06 22:04:29 +0530

    create get user server action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -22,3 +22,12 @@ export const createUser = async (user: CreateUserParams) => {
         console.error("An error occurred while creating a new user:", error);
     }
 }
+
+export const getUser = async (userId: string) => {
+    try {
+        const user = await users.get(userId);
+        return parseStringify(user);
+    } catch (error) {
+        console.error("An error occurred while retrieving the user details:", error);
+    }
+};

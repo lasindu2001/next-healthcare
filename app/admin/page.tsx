e2c17c216fe6b80601a48c9b2bb86c790ce05c42
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-08-10 17:07:33 +0530

    get recent appointment  server action

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 import StatCard from "@/components/StatCard"
 import DataTable from "@/components/table/DataTable"
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Admin = () => {
+const Admin = async () => {
+  const appointments = await getRecentAppointmentList();
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -28,19 +31,19 @@ const Admin = () => {
         <section className="admin-stat">
           <StatCard
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Scheduled appointments"
             icon={"/assets/icons/appointments.svg"}
           />
           <StatCard
             type="pending"
-            count={5}
+            count={appointments.pendingCount}
             label="Pending appointments"
             icon={"/assets/icons/pending.svg"}
           />
           <StatCard
             type="cancelled"
-            count={5}
+            count={appointments.cancelledCount}
             label="Cancelled appointments"
             icon={"/assets/icons/cancelled.svg"}
           />
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,9 +1,10 @@
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { config, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -32,3 +33,59 @@ export const getAppointment = async (appointmentId: string) => {
         console.error("An error occurred while retrieving the existing patient:", error);
     }
 };
+
+export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            config.DATABASE_ID!,
+            config.APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc("$createdAt")]
+        );
+        // const scheduledAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "scheduled");
+        // const pendingAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "pending");
+        // const cancelledAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "cancelled");
+        // const data = {
+        //   totalCount: appointments.total,
+        //   scheduledCount: scheduledAppointments.length,
+        //   pendingCount: pendingAppointments.length,
+        //   cancelledCount: cancelledAppointments.length,
+        //   documents: appointments.documents,
+        // };
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        };
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+                switch (appointment.status) {
+                    case "scheduled":
+                        acc.scheduledCount++;
+                        break;
+                    case "pending":
+                        acc.pendingCount++;
+                        break;
+                    case "cancelled":
+                        acc.cancelledCount++;
+                        break;
+                }
+                return acc;
+            },
+            initialCounts
+        );
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents,
+        };
+        return parseStringify(data);
+    } catch (error) {
+        console.error("An error occurred while retrieving the recent appointments:", error);
+    }
+}

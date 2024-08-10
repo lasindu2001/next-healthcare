62d5c845081e809db100d9cdd8adcef42787d761
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-08-10 14:19:09 +0530

    create statcard component

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import Image from "next/image";
+
 type StatCardProps = {
     type: "appointments" | "pending" | "cancelled";
     count: number;
@@ -7,7 +10,24 @@ type StatCardProps = {
 
 const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
     return (
-        <div>StatCard</div>
+        <div className={clsx("stat-card", {
+            "bg-appointments": type === "appointments",
+            "bg-pending": type === "pending",
+            "bg-cancelled": type === "cancelled",
+        })}
+        >
+            <div className="flex items-center gap-4">
+                <Image
+                    src={icon}
+                    height={32}
+                    width={32}
+                    alt="appointments"
+                    className="size-8 w-fit"
+                />
+                <h2 className="text-32-bold text-white">{count}</h2>
+            </div>
+            <p className="text-14-regular">{label}</p>
+        </div>
     )
 }
 
